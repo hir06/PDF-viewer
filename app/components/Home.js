4f41a437ba616e9dc77891c10baee92324948101
hparmar@gainsight.com <hparmar@gainsight.com>
2019-09-18 23:29:37 +0530

    pdf reader added

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,45 +2,76 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import routes from '../constants/routes';
-import styles from './Home.css';
+import styles from './Home.scss';
 import PdfViewer from './PdfViewer';
 type Props = {};
 
-export default class Home extends Component<Props> {
-  props: Props;
-  constructor (props) {
-    super(props);
-  }
-  handleClick(e) {
-    this.refs.fileUploader.click();
-  }
-  onChangeFile(event) {
-    event.stopPropagation();
-    event.preventDefault();
-    var file = event.target.files[0];
-    console.log(file);
-    this.setState({file}); /// if you want to upload latter
-}
-  showOpenFileDlg = () => {
-    this.inputOpenFileRef.current.click()
-}
-  render() {
-    return (
-            <div className={styles.frame} data-tid="container">
-            <div className={styles.leftBar}>
-            <span className={styles.files}>Files</span>
-            <div className={styles.docList}>
-              <div className={styles.docContainer}>
-              
-              </div>
-            </div>
-            <input type="file" id="file" ref="fileUploader" onChange={this.onChangeFile.bind(this)} style={{display: "none"}}/>
-            <button onClick={this.handleClick.bind(this)} className={styles.upload}><span className={styles.icon}></span>Upload Files</button>
-            </div>
-            <div className={styles.rightBar}>
-            <PdfViewer />
-            </div>
-            </div>
-    );
-  }
+export default class Home extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            docList: ['hiral'],
+            activeItem : {}
+        }
+        this.onChangeFile = this.onChangeFile.bind(this);
+    }
+
+    handleClick(e) {
+        this.refs.fileUploader.click();
+    }
+    onChangeFile = (event) => {
+        event.stopPropagation();
+        event.preventDefault();
+        var file = event.target.files[0];
+        var docListTemp = this.state.docList;
+        docListTemp.push(file.name);
+        this.setState({ docList: docListTemp });
+        console.log(this.state.docList);
+    }
+  
+    showOpenFileDlg = () => {
+        this.inputOpenFileRef.current.click();
+    }
+    
+
+    render() {
+        let listOfFiles = this.state.docList.map(function(item, index) { 
+        return (<div key={index} className = {styles.leftMenuButton}>
+            <div className = { styles.icon }></div> 
+            <div> < span className = { styles.docName } > { item } </span>  </div >
+    </div> )
+        });
+        return ( <
+            div className = { styles.frame } >
+            <
+            div className = { styles.leftBar } >
+            <
+            div className = { styles.readerZone } > < /div> <
+            span className = { styles.files } > Files < /span> <
+            div className = { styles.docList } > { listOfFiles } <
+            /div> <
+            input type = "file"
+            id = "file"
+            ref = "fileUploader"
+            onChange = { this.onChangeFile.bind(this) }
+            style = {
+                { display: "none" }
+            }
+            /> <
+            button onClick = { this.handleClick.bind(this) }
+            className = { styles.upload } >
+            <
+            span className = { styles.icon } > < /span> <
+            span > Upload Files < /span> < /
+            button > <
+            /div> <
+            div className = { styles.rightBar } >
+            <
+            PdfViewer listNameFromParent={this.state.activeItem}/ >
+            <
+            /div> < /
+            div >
+        );
+    }
 }
--- app/components/PdfViewer.js
+++ app/components/PdfViewer.js
@@ -2,12 +2,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import routes from '../constants/routes';
-import styles from './PdfViewer.css';
+import styles from './Pdfviewer.scss';
 //import { Document } from 'react-pdf/dist/entry.webpack';
 // import { Document, Page } from 'react-pdf/build/entry.noworker';
 import { Document, Page, setOptions } from 'react-pdf';
 import { pdfjs } from 'react-pdf';
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = `http://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 // import { Document } from 'react-pdf/dist/entry.webpack';
 import samplePDF from '../utils/hiral_resume.pdf';
 //setOptions({ workerSrc: '/pdf.worker.min.js', });
@@ -30,16 +30,31 @@ export default class PdfViewer extends Component<Props> {
     const { pageNumber, numPages } = this.state;
 
     return (
-            <div className={styles.frame} data-tid="container">
-        <Document
-          file={samplePDF}
-          onLoadSuccess={this.onDocumentLoadSuccess}
-        >
-          <Page pageNumber={pageNumber} />
-        </Document>
-        <p>Page {pageNumber} of {numPages}</p>
-  
-            </div>
+        <div>
+              <div className={styles.title}>
+              <div className={styles.icon}></div>
+              <span className={styles.name}>Document 1</span>
+              </div>  
+              <div className={styles.container}>
+              <nav>
+          <button onClick={this.goToPrevPage}>Prev</button>
+          <button onClick={this.goToNextPage}>Next</button>
+        </nav>
+
+        <div style={{ width: 600 }}>
+          <Document
+            file={samplePDF}
+            onLoadSuccess={this.onDocumentLoadSuccess}
+          >
+            <Page pageNumber={pageNumber} width={600} />
+          </Document>
+        </div>
+
+        <p>
+          Page {pageNumber} of {numPages}
+        </p>
+              </div>
+        </div>
     );
   }
 }
